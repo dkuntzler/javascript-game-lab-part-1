6b4eb4e4867b5cb37c96c142c2f8d208e9fae303
Dana Lynn Kuntzler <Dana@Danas-MacBook-Pro.local>
2019-02-20 18:17:57 -0500

    second commit

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-const answer = prompt("Do You Want To Play?");
-  if (answer === "yes") {
-    const name = prompt("What Is Your Character's Name?");
-    
+
+ function startGame() { 
+  const answer = prompt("Do You Want To Play?");
+   if (answer === "yes")  {
+    const username = prompt("What Is Your Character's Name?");
+    startCombat(username);
+}   else if (answer === "no") {
+  const startCombat = alert("Too bad!");
+ }
+}
+  
+   function startCombat(username) { 
     let health = 40;
     let grant = 10;
     let wins = 0;
-
-    while (grant > 0 && wins < 3) {
-        health -= (Math.floor((Math.random() * 2) + 1));
-        grant -=(Math.floor((Math.random() * 2) + 1));
-      console.log(`${name} has ${health} health left`);
-      console.log(`Grant the Mighty Chicken has ${grant} health left`);
+ 
+    function getDamage() { 
+      health -= (Math.floor((Math.random() * 5) + 1));
+      grant -=(Math.floor((Math.random() * 5) + 1)); 
+    console.log(`${username} has ${health} health left`);
     }
+      
+      while (grant > 0 && health > 0 && wins < 3){
+        const attack = prompt("Would you like to attack or quit?");
+        if (attack === "attack"){
+
+          console.log(`Dana chose to attack`);
+          getDamage();
+      console.log(`Grant the Mighty Chicken has ${grant} health left`);
+        } else if (attack ==="quit") {
+          console.clear();
+         break;
+        }
+    
     if(grant <= 0) {
       wins++; grant=10;
-      console.log(`${name} has won the game!`)
+      console.log(`${username} has won the game!`);
+      //alert(`${username} has won the game!`);
+     
+
+    } else if (health <= 0) {
+      console.log(`${username} has lost the game!`)
     }
+  }
+  }
+
+ 
+
   
-
-
-
-  }
-  else if (answer === "no") {
-    const name = alert("Too bad!");
-  }
+  /*else (startGame === "no") {
+    const startCombat = alert("Too bad!");
+  }*/
